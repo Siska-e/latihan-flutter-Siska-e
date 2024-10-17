8418808858a49bb6edf3f6d90e497f7d6f19a91f
Siska-e <siskaamelia_2327250062@mhs.mdp.ac.id>
2024-10-17 15:15:27 +0700

    commit 1

---
+++ wisata_candi/lib/detail_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_back,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(candi.name),
+                  IconButton(onPressed: () {}, 
+                  icon: Icon(Icons.favorite_border)),
+                ],
+              )
+            ],
+          ),),
+          Text(candi.name),
+          Text(candi.description),
+        ],
+      ),
+    );
+  }
+}
